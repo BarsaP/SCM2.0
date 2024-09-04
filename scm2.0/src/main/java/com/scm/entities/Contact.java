ca3a5ab1444e2035de0e2c3e00908b428469541a
barsa <barshapriyadarshini0602@gmail.com>
2024-09-04 23:19:53 +0530

    addition

---
+++ scm2.0/src/main/java/com/scm/entities/Contact.java
@@ -0,0 +1,15 @@
+package com.scm.entities;
+
+public class Contact {
+  
+	private String id;
+	private String name;
+	private String email;
+	private String phoneNumber;
+	private String address;
+	private String picture;
+	private String description;
+	private boolean favorite = false;
+	
+	
+}
---
+++ scm2.0/src/main/resources/static/js/script.js
@@ -0,0 +1,60 @@
+console.log("Script loaded");
+
+let currentTheme = getTheme();
+//initial -->
+changeTheme();
+
+//Todo
+function changeTheme(){
+	//set to web page
+	//changePageTheme(currentTheme, currentTheme);
+	//document.querySelector("html").classList.add(currentTheme);
+	
+	//set the listener to change theme button
+	const changeThemeButton = document.querySelector('#theme_change_button');
+	//changeThemeButton.querySelector("span").textContent = 
+	//	currentTheme == "light" ? "Dark" : "Light";
+	changeThemeButton.addEventListener('click', (event) => {
+		
+		console.log("change theme button clicked");
+		
+		let oldTheme = currentTheme;
+		
+		if(currentTheme == "dark"){
+			//theme light
+			currentTheme = "light";
+		}else{
+			//theme dark
+			currentTheme = "dark";
+		}
+		//update in localStroage
+		changePageTheme(currentTheme, oldTheme);
+	});
+}
+
+//set theme to localstorage
+//localstorage store the data
+function setTheme(theme){
+	localStorage.setItem("theme", theme);
+}
+
+//get theme from localstorage
+function getTheme(){
+	let theme = localStorage.getItem("theme");
+	//tornary operator
+	return theme ? theme : "light";
+}
+
+//change current page theme
+function changePageTheme(theme, oldTheme){
+	setTheme(currentTheme);
+	//remove the current theme
+	document.querySelector("html").classList.remove(oldTheme);
+			
+	//set the current theme
+	document.querySelector("html").classList.add(theme);
+			
+	//change the text of button
+	document.querySelector('#theme_change_button').querySelector("span").textContent = 
+	theme == "light" ? "Dark" : "Light";
+}
